f2f0437cbde9bf7e5bfb244788aaf5d12b87881a
HoangMinh23062002 <minh.hoang23@student.passerellesnumeriques.org>
2022-05-31 10:06:45 +0700

    donefilter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './filter.css'
+
+
+const USERS = [
+    { id: 1, name: 'Tú', age: 20 },
+    { id: 2, name: 'Lum Lum', age: 19 },
+    { id: 3, name: 'Thị Phượng', age: 21 },
+    { id: 4, name: 'Zũ', age: 20 },
+    { id: 5, name: 'Tờ rí', age: 21 },
+    { id: 6, name: 'Ngài', age: 20 },
+    { id: 7, name: 'Hứ', age: 19 },
+    { id: 8, name: 'Điệu', age: 20 },
+];
+
+function Filter() {
+    // the value of the search field 
+    const [name, setName] = useState('');
+
+    // the search result
+    const [foundUsers, setFoundUsers] = useState(USERS);
+
+    const filter = (e) => {
+        const keyword = e.target.value;
+
+        if (keyword !== '') {
+            const results = USERS.filter((user) => {
+                return user.name.toLowerCase().startsWith(keyword.toLowerCase());
+                // Use the toLowerCase() method to make it case-insensitive
+            });
+            setFoundUsers(results);
+        } else {
+            setFoundUsers(USERS);
+            // If the text field is empty, show all users
+        }
+
+        setName(keyword);
+    };
+
+    return (
+        <div className="container">
+            <input
+                type="text"
+                value={name}
+                onChange={filter}
+                className="input"
+                placeholder="Search..."
+            />
+
+            <div className="user-list">
+                {foundUsers && foundUsers.length > 0 ? (
+                    foundUsers.map((user) => (
+                        <li key={user.id} className="user">
+                            <span className="user-id">{user.id}</span>
+                            <span className="user-name">{user.name}</span>
+                            <span className="user-age">{user.age} year old</span>
+                        </li>
+                    ))
+                ) : (
+                    <h1>No results found!</h1>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default Filter;
